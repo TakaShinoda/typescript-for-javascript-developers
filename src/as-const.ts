c8e7d5aa872e873d888062e3385cc0597d6af33a
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-25 13:15:30 +0900

    Add as-const.ts

---
+++ src/as-const.ts
@@ -0,0 +1,16 @@
+export {}
+
+let name = 'Taro'
+
+name = 'Jiro'
+
+let nickname = 'Jiro' as const
+nickname = 'Jiro'
+
+let profile = {
+    name: 'Taro',
+    height: 189
+} as const
+
+// profile.name = 'Koji'
+// profile.height = 180
